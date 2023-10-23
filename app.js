91d3256dfc501379406744f6d96dd63379f54697
DanielViero <daniele.viero@gruner.ch>
2023-10-23 09:16:42 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0x666565) });
+
+// Create grid and axes
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+		// Load the model
+    const model = await viewer.IFC.loadIfcUrl(url);
+    const pos = getCenterPoint(model.modelID.mesh); // not triggered
+    pick()
+		// Add dropped shadow and post-processing efect
+    await viewer.shadowDropper.renderShadow(model.modelID);
+    viewer.context.renderer.postProduction.active = true;
+
+}
+
+function getCenterPoint(mesh) {
+    var geometry = mesh.geometry;
+    geometry.computeBoundingBox();
+    var center = new THREE.Vector3();
+    geometry.boundingBox.getCenter( center );
+    mesh.localToWorld( center );
+    return center;
+  }
+loadIfc('./Corridor_solids.ifc');
